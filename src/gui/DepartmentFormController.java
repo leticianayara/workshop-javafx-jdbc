073b7ac7a136f087a138cb46c9f4ffa7778d8682
Leticia Costa <leticianayaralima@gmail.com>
2025-02-27 17:48:15 -0300

    Passando um objeto Department para o formulario

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,9 +9,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable{
 
+	private Department entity;
+	
 	@FXML
 	private TextField txtId; 
 	
@@ -41,6 +44,19 @@ public class DepartmentFormController implements Initializable{
 	public void initialize(URL url, ResourceBundle rb) {
 		initializNode();
 	}
+	
+	public void setEntity(Department entity) {
+		this.entity = entity;
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
 
 	private void initializNode() {
 		Constraints.setTextFieldInteger(txtId);
