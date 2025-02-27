ce3b66f3cd6c4b11669956f884d34801a2b49b5e
Leticia Costa <leticianayaralima@gmail.com>
2025-02-27 10:09:57 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
@@ -12,6 +11,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primarySatege) {
 		try {
@@ -22,13 +23,17 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);
 			
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primarySatege.setScene(mainScene);
 			primarySatege.setTitle("Sample JavaFX application");
 			primarySatege.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
